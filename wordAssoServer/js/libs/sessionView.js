e010141515187c5167d5a68d05e5c407df8a375a
tracycollins <tc@threeceemedia.com>
2016-03-25 01:33:04 -0400

    defaultRadiusScale

--- wordAssoServer/js/libs/sessionView.js
+++ wordAssoServer/js/libs/sessionView.js
@@ -558,7 +558,7 @@ function launchSessionView(sessionId) {
 var adjustedAgeRateScale = d3.scale.pow().domain([1,500]).range([1.0,100.0]);
 var fontSizeScale = d3.scale.log().domain([1,100000]).range([1.6,3.2]);
 
-var defaultRadiusScale = d3.scale.log().domain([1,1000000]).range([1.0,40.0]);
+var defaultRadiusScale = d3.scale.log().domain([1,2000000]).range([1.0,40.0]);
 
 var fillColorScale = d3.scale.linear()
     .domain([0, 30000, 60000])

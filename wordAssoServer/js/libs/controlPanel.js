9a756bd5c7040210cf2af639ba70d04a007e4fb2
tracycollins <tc@threeceemedia.com>
2016-11-03 19:15:57 -0400

    control panel by host

--- wordAssoServer/js/libs/controlPanel.js
+++ wordAssoServer/js/libs/controlPanel.js
@@ -6,7 +6,8 @@
 function ControlPanel() {
 
   // var DEFAULT_SOURCE = "http://localhost:9997";
-  var DEFAULT_SOURCE = "http://word.threeceelabs.com";
+  // var DEFAULT_SOURCE = "http://word.threeceelabs.com";
+  var DEFAULT_SOURCE = "==SOURCE==";  // will be updated by wordAssoServer.js on app.get
 
   var parentWindow;
   var self = this;

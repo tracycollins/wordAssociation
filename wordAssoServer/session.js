e9a320a294dc4799a54dd1df7ce51fd16334950e
tracycollins <tc@threeceemedia.com>
2016-12-22 11:06:52 -0500

    msToTime days

--- wordAssoServer/session.js
+++ wordAssoServer/session.js
@@ -288,16 +288,18 @@ ignoreWordsArray.push('"');
 ignoreWordsArray.push("'");
 
 function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
+  var milliseconds = parseInt((duration % 1000) / 1000),
     seconds = parseInt((duration / 1000) % 60),
     minutes = parseInt((duration / (1000 * 60)) % 60),
-    hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+    hours = parseInt((duration / (1000 * 60 * 60)) % 24),
+    days = parseInt(duration / (1000 * 60 * 60 * 24));
 
+  days = (days < 10) ? "0" + days : days;
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-  return hours + ":" + minutes + ":" + seconds;
+  return days + ":" + hours + ":" + minutes + ":" + seconds;
 }
 
 function displayControl(isVisible) {

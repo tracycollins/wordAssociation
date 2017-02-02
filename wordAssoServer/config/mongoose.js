29440d54735cee8a9cd0805632fcd772a51c4738
tracycollins <tc@threeceemedia.com>
2017-02-01 20:28:08 -0500

    mongodb config

--- wordAssoServer/config/mongoose.js
+++ wordAssoServer/config/mongoose.js
@@ -1,26 +1,56 @@
 var config = require('./config'),
 	mongoose = require('mongoose');
 
-var options = { 
-	server: { 
-		auto_reconnect: true,
-		poolSize: 20,
-		socketOptions: { 
-			// reconnectTries: Number.MAX_VALUE,
-			keepAlive: 1,
-			// keepAlive: 300000,
-			// socketTimeoutMS: 180000,
-			// connectTimeoutMS: 180000 
+module.exports = function() {
+	var options = { 
+		server: { 
+      auto_reconnect: true,
+			poolSize: 50,
+			socketOptions: { 
+				reconnectTries: 1000,
+				keepAlive: 1000,
+				socketTimeoutMS: 180000,
+				connectTimeoutMS: 180000 
+			},
 		},
-	},
-	db: {
+    db: {
 	    numberOfRetries: 20,
 	    retryMiliSeconds: 1000
-	}
-};
+    }
+	};
 
-module.exports = function() {
-	var wordAssoDb = mongoose.connect(config.wordAssoDb, options);
+// module.exports = function() {
+	// var wordAssoDbwordAssoDb = mongoose.connect(config.wordAssoDb, options);
+
+	var wordassOdb = mongoose.connect(config.wordassOdb, options, function(error){
+		if (error) {
+			console.log('CONNECT FAILED: ERROR: MONGOOSE default connection open to ' + config.wordassOdb);
+		}
+		else {
+			console.log('CONNECT: MONGOOSE default connection open to ' + config.wordassOdb);
+		}
+	});
+
+	// CONNECTION EVENTS
+	// When successfully connected
+	wordassOdb.connection.on('connected', function () {  
+	  console.log('MONGOOSE default connection OPEN to ' + config.wordassOdb);
+	}); 
+
+	wordassOdb.connection.on('close', function () {  
+	  console.log('MONGOOSE default connection CLOSED to ' + config.wordassOdb);
+	}); 
+
+	wordassOdb.connection.on('error', function (err) {
+		console.log("MONGOOSE ERROR\n" + err);
+		// if (mongoose.connection.readyState == 0) wordassOdb = mongoose.connect(config.wordassOdb, options);
+	});
+
+	// When the connection is disconnected
+	wordassOdb.connection.on('disconnected', function (err) {  
+	  console.log('MONGOOSE default connection disconnected\n' + err);
+	});
+
 
 	require('../app/models/admin.server.model');  
 	require('../app/models/viewer.server.model');  

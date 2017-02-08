b1280af12b76abc9a298a613bdb6e0078a755c27
tracycollins <tc@threeceemedia.com>
2017-02-08 13:11:17 -0500

    initialPosition, stats

--- wordAssoServer/session.js
+++ wordAssoServer/session.js
@@ -793,17 +793,19 @@ var initialXpositionRatio = 0.7;
 var initialYpositionRatio = 0.4;
 
 function computeInitialPosition(index) {
-  var radiusX = 0.2*currentSessionView.getWidth();
-  var radiusY = 0.2*currentSessionView.getHeight();
+  // var radiusX = 0.2*currentSessionView.getWidth();
+  // var radiusY = 0.2*currentSessionView.getHeight();
   // var radiusX = 0.1*width;
   // var radiusY = 0.1*height;
   var pos = {
     // x: ((initialXpositionRatio * 1000) + (radiusX * Math.cos(index))),
     // y: ((initialYpositionRatio * 1000) + (radiusY * Math.cos(index)))
-    x: ((initialXpositionRatio * currentSessionView.getWidth()) - (radiusX * Math.abs(Math.cos(index)))),
-    y: ((initialYpositionRatio * currentSessionView.getHeight()) + (radiusY * Math.cos(index)))
+    // x: ((initialXpositionRatio * currentSessionView.getWidth()) - (radiusX * Math.abs(Math.cos(index)))),
+    // y: ((initialYpositionRatio * currentSessionView.getHeight()) + (radiusY * Math.cos(index)))
     // x: 500,
     // y: 500
+    x: randomIntFromInterval(0.7 * currentSessionView.getWidth(), 0.75 * currentSessionView.getWidth()),
+    y: randomIntFromInterval(0.3 * currentSessionView.getHeight(), 0.7 * currentSessionView.getHeight())
   };
 
   return pos;
@@ -1665,9 +1667,12 @@ function updateStatsTable(statsObj){
   document.getElementById("statsServerRunTime").innerHTML = msToTime(statsObj.heartbeat.runTime);
   document.getElementById("statsServerTotalWords").innerHTML = statsObj.heartbeat.totalWords;
   document.getElementById("statsServerWordsReceived").innerHTML = statsObj.heartbeat.responsesReceived;
-  // document.getElementById("statsServerWordsPerMin").innerHTML = statsObj.heartbeat.wordsPerMinute.toFixed(2);
-  // document.getElementById("statsServerMaxWordsPerMin").innerHTML = statsObj.heartbeat.maxWordsPerMin.toFixed(2);
+
+  document.getElementById("statsServerWordsPerMin").innerHTML = statsObj.heartbeat.wordsPerMinute.toFixed(2);
+  document.getElementById("statsServerMaxWordsPerMin").innerHTML = statsObj.heartbeat.maxWordsPerMin.toFixed(2);
+
   document.getElementById("statsServerMaxWordsPerMinTime").innerHTML = moment(statsObj.heartbeat.maxWordsPerMinTime).format(defaultDateTimeFormat);
+
   document.getElementById("statsClientNumberNodes").innerHTML = currentSessionView.getNodesLength();
   document.getElementById("statsClientNumberMaxNodes").innerHTML = statsObj.maxNodes;
   document.getElementById("statsClientNumberEntities").innerHTML = sessionHashMap.count();

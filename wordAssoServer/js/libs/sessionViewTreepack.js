7f1e9939851ae269aeb049c6fc10eeb662e24227
tracycollins <tc@threeceemedia.com>
2017-04-04 19:18:29 -0400

    remove foci

--- wordAssoServer/js/libs/sessionViewTreepack.js
+++ wordAssoServer/js/libs/sessionViewTreepack.js
@@ -386,28 +386,28 @@ function ViewTreepack() {
         node.ageMaxRatio = ageMaxRatio;
         node.isDead = false;
 
-        if (node.isKeyword){
-          if (node.keywords.right !== undefined) {
-            node.y += (foci.right.y - node.y) * 0.1* simulation.alpha();
-            node.x += (foci.right.x - node.x) * 0.1* simulation.alpha();
-          }
-          if (node.keywords.left !== undefined) {
-            node.y += (foci.left.y - node.y) * 0.1* simulation.alpha();
-            node.x += (foci.left.x - node.x) * 0.1* simulation.alpha();
-          }
-          if (node.keywords.positive !== undefined) {
-            node.y += (foci.positive.y - node.y) * 0.1* simulation.alpha();
-            node.x += (foci.positive.x - node.x) * 0.1* simulation.alpha();
-          }
-          if (node.keywords.negative !== undefined) {
-            node.y += (foci.negative.y - node.y) * 0.1* simulation.alpha();
-            node.x += (foci.negative.x - node.x) * 0.1* simulation.alpha();
-          }
-        }
-        else {
-          node.y += (foci.default.y - node.y) * 0.1* simulation.alpha();
-          node.x += (foci.default.x - node.x) * 0.1* simulation.alpha();
-        }
+        // if (node.isKeyword){
+        //   if (node.keywords.right !== undefined) {
+        //     node.y += (foci.right.y - node.y) * 0.1* simulation.alpha();
+        //     node.x += (foci.right.x - node.x) * 0.1* simulation.alpha();
+        //   }
+        //   if (node.keywords.left !== undefined) {
+        //     node.y += (foci.left.y - node.y) * 0.1* simulation.alpha();
+        //     node.x += (foci.left.x - node.x) * 0.1* simulation.alpha();
+        //   }
+        //   if (node.keywords.positive !== undefined) {
+        //     node.y += (foci.positive.y - node.y) * 0.1* simulation.alpha();
+        //     node.x += (foci.positive.x - node.x) * 0.1* simulation.alpha();
+        //   }
+        //   if (node.keywords.negative !== undefined) {
+        //     node.y += (foci.negative.y - node.y) * 0.1* simulation.alpha();
+        //     node.x += (foci.negative.x - node.x) * 0.1* simulation.alpha();
+        //   }
+        // }
+        // else {
+        //   node.y += (foci.default.y - node.y) * 0.1* simulation.alpha();
+        //   node.x += (foci.default.x - node.x) * 0.1* simulation.alpha();
+        // }
 
         nodes[ageNodesIndex] = node;
         localNodeHashMap.set(node.nodeId, node);

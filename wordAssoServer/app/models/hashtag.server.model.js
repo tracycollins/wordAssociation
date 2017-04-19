cfea71c110b1d6d0d802abc31cb90756e4b3a9ec
tracycollins <tc@threeceemedia.com>
2017-04-19 19:45:13 -0400

    metrics deleteArray work

---
+++ wordAssoServer/app/models/hashtag.server.model.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose'),
+	crypto = require('crypto'),
+	Schema = mongoose.Schema;
+
+var HashtagSchema = new Schema({
+	hashtagId: { 
+		type: String
+	},
+	nodeType: { 
+		type: String,
+		default: 'hashtag'
+	},
+	nodeId: { // for D3.js
+		type: String
+	},
+	text: { 
+		type: String,
+		unique: true
+	},
+	lastSeen: {   // in ms
+		type: String
+	},
+	isTopTerm: {
+		type: Boolean,
+		default: false
+	},
+	isTrendingTopic: {
+		type: Boolean,
+		default: false
+	},
+	mentions: {
+		type: Number,
+		default: 0
+	}
+});
+
+
+mongoose.model('Hashtag', HashtagSchema);

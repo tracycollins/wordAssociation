1c57190a53a9f1f2fcb060817f99eabce731696c
tracycollins <tc@threeceemedia.com>
2017-05-13 21:25:33 -0400

    socket.io

--- wordAssoServer/session.js
+++ wordAssoServer/session.js
@@ -15,7 +15,7 @@ when new instance of word arrives, iterate thru array of nodes and create linksk
 // var DEFAULT_SOURCE = "http://word.threeceelabs.com";
 var DEFAULT_SOURCE = "==SOURCE==";  // will be updated by wordAssoServer.js on app.get
 
-var socket;
+var socket = io("view");
 
 var PARENT_ID = "0047";
 

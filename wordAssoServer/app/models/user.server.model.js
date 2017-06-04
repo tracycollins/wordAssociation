33d40359a5a19c54962d736e3c223ae22f1d94ce
tracycollins <tc@threeceemedia.com>
2017-06-04 13:59:52 -0400

    user model; shorter intervals

--- wordAssoServer/app/models/user.server.model.js
+++ wordAssoServer/app/models/user.server.model.js
@@ -4,111 +4,46 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-	userId: { 
-		type: String,
-		trim: true,
-		unique: true
-	},
-	name: {
-		type: String
-	},
-	screenName: {
-		type: String,
-		trim: true
-	},
-	description: { 
-		type: String,
-		trim: true
-	},
-	url: { 
-		type: String,
-		trim: true
-	},
-	profileUrl: { 
-		type: String,
-		trim: true
-	},
-	profileImageUrl: { 
-		type: String,
-		trim: true
-	},
-	tags: { 
-		type: Object,
-		default: {}
-	},
-	isTopTerm: { 
-		type: Boolean,
-		default: false
-	},
-	rate: {
-		type: Number,
-		default: 0
-	},
-	verified: { 
-		type: Boolean,
-		default: false
-	},
-	createdAt: { 
-		type: Number,
-		default: moment().valueOf()
-	},
-	lastSeen: {   
-		type: Number,
-		default: moment().valueOf()
-	},
-	connected: { 
-		type: Boolean,
-		default: false
-	},
-	connectTime: { 
-		type: Number,
-		default: 0
-	},
-	disconnectTime: { 
-		type: Number,
-		default: 0
-	},
-	sessionId: {   // ??????? KLUDGE: for admin updates of sessions/users/admins
-		type: String,
-		trim: true
-	},
-	namespace: {   // ??????? KLUDGE: for admin updates of sessions/users/admins
-		type: String,
-		trim: true
-	},
-	ip: {   // ??????? KLUDGE: for admin updates of sessions/users/admins
-		type: String,
-		trim: true
-	},
-	domain: {   // ??????? KLUDGE: for admin updates of sessions/users/admins
-		type: String,
-		trim: true
-	},
-	lastSession: {   
-		type: String,
-		trim: true
-	},
-	statusesCount: {
-		type: Number,
-		default: 0
-	},
-	followersCount: {
-		type: Number,
-		default: 0
-	},
-	friendsCount: {
-		type: Number,
-		default: 0
-	},
-	mentions: {
-		type: Number,
-		default: 0
-	},
-	sessions: {  
-		type: [String],// Session objects
-		trim: true
-	}
-});
+	userId: { type: String,	unique: true },
+	nodeId: { type: String	},
+	nodeType: { type: String, default: "user"},
+	name: { type: String },
+	screenName: { type: String },
+	url: { type: String	},
+	profileUrl: { type: String },
+	profileImageUrl: { type: String	},
+	verified: { type: Boolean, default: false },
+	description: { type: String },
 
+	createdAt: { type: Number, default: moment().valueOf() },
+	lastSeen: { type: Number, default: moment().valueOf() },
+
+	ip: { type: String },
+	domain: { type: String },
+	namespace: { type: String },
+	connected: { type: Boolean, default: false },
+	connectTime: { type: Number, default: 0 },
+	disconnectTime: { type: Number, default: 0 },
+	sessionId: { type: String },
+	sessions: { type: [ String ],	trim: true },
+	lastSession: { type: String },
+
+	tags: { type: Object, default: {} },
+
+	mentions: { type: Number, default: 0 },
+
+	status: { type: Object, default: {} },
+	statusesCount: { type: Number, default: 0 },
+	followersCount: { type: Number, default: 0 },
+	following: { type: Boolean,	default: false },
+	friendsCount: { type: Number, default: 0 },
+
+	rate: { type: Number, default: 0 },
+	isTopTerm: { type: Boolean, default: false },
+	keywords: { type: Object, default: {} },
+	keywordsAuto: { type: Object, default: {} },
+
+	languageAnalysis: { type: Object, default: {} }
+});
 
 mongoose.model('User', UserSchema);

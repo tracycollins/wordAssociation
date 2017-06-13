2da2947a89877610e67e153bd87b203aa50cc898
tracycollins <tc@threeceemedia.com>
2017-06-13 19:26:00 -0400

    "ENABLE_GOOGLE_METRICS": false mem leak debug

--- wordAssoServer/wordAssoServer02.js
+++ wordAssoServer/wordAssoServer02.js
@@ -4,6 +4,16 @@
 console.log("\n\n============== START ==============\n\n");
 
 var quitOnError = true;
+
+var pmx = require('pmx').init({
+  http          : true, // HTTP routes logging (default: true)
+  ignore_routes : [/socket\.io/, /notFound/], // Ignore http routes with this pattern (Default: [])
+  errors        : true, // Exceptions logging (default: true)
+  custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+  network       : true, // Network monitoring at the application level
+  ports         : true  // Shows which ports your app is listening on (default: false)
+});
+
 // ==================================================================
 // GLOBAL VARIABLES
 // ==================================================================
@@ -3147,7 +3157,6 @@ function initStatsInterval(interval){
 
   }, interval);
 }
-
 
 //=================================
 // BEGIN !!

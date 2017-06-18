bae70a10ef1c7a714797a0c6ec5f28703027fcae
tracycollins <tc@threeceemedia.com>
2017-06-18 09:24:28 -0400

    enable heapdump

--- wordAssoServer/wordAssoServer02.js
+++ wordAssoServer/wordAssoServer02.js
@@ -7,9 +7,9 @@ console.log("PROCESS PID: " + process.pid);
 
 var quitOnError = true;
 
-var enableHeapDump = false;
+var enableHeapDump = true;
 var heapdumpStatsModulo = 10;
-// var heapdump = require('heapdump');
+var heapdump = require('heapdump');
 
 // var memwatch = require('memwatch');
 
@@ -3128,21 +3128,21 @@ function initStatsInterval(interval){
 
     statsUpdated += 1;
 
-    // if (enableHeapDump && (statsUpdated > 1) && (statsUpdated % heapdumpStatsModulo == 0)) {
+    if (enableHeapDump && (statsUpdated > 1) && (statsUpdated % heapdumpStatsModulo == 0)) {
 
-    //   heapdumpFileName = "was2" 
-    //     + "_" + hostname 
-    //     + "_" + process.pid 
-    //     + "_" + moment().format(tinyDateTimeFormat) 
-    //     + ".heapsnapshot";
+      heapdumpFileName = "was2" 
+        + "_" + hostname 
+        + "_" + process.pid 
+        + "_" + moment().format(tinyDateTimeFormat) 
+        + ".heapsnapshot";
 
-    //   console.error(chalkError("***** HEAPDUMP *****"
-    //     + " | STATS UPDATED: " +  statsUpdated
-    //     + " | FILE: " +  heapdumpFileName
-    //   ));
+      console.error(chalkError("***** HEAPDUMP *****"
+        + " | STATS UPDATED: " +  statsUpdated
+        + " | FILE: " +  heapdumpFileName
+      ));
 
-    //   heapdump.writeSnapshot(heapdumpFileName);
-    // }
+      heapdump.writeSnapshot(heapdumpFileName);
+    }
 
   }, interval);
 }

7701813b6bd5c90b1fba23732c84eaebb7c25d81
tracycollins <tc@threeceemedia.com>
2017-07-02 01:46:41 -0400

    new mongoose connection
    const wordAssoDb = mongoose.createConnection

--- wordAssoServer/config/mongoose.js
+++ wordAssoServer/config/mongoose.js
@@ -1,10 +1,13 @@
-var config = require('./config'),
-	mongoose = require('mongoose');
+/*jslint node: true */
+"use strict";
+
+const config = require("./config");
+const mongoose = require("mongoose");
 
 
 module.exports = function() {
 
-	var options = { 
+	const options = { 
 		server: { 
       auto_reconnect: true,
 			poolSize: 20,
@@ -21,54 +24,59 @@ module.exports = function() {
 	    retryMiliSeconds: 1000
     }
 	};
-	var wordAssoDb = mongoose.connect(config.wordAssoDb, options, function(error){
+
+
+// const conn = mongoose.createConnection(...)
+// const Model = conn.model(...)
+
+	const wordAssoDb = mongoose.createConnection(config.wordAssoDb, options, function(error){
 		if (error) {
-			console.log('CONNECT FAILED: ERROR: MONGOOSE default connection open to ' + config.wordAssoDb);
+			console.log("CONNECT FAILED: ERROR: MONGOOSE default connection open to " + config.wordAssoDb);
 		}
 		else {
-			console.log('CONNECT: MONGOOSE default connection open to ' + config.wordAssoDb);
+			console.log("CONNECT: MONGOOSE default connection open to " + config.wordAssoDb);
 		}
 	});
 
 	// CONNECTION EVENTS
 	// When successfully connected
-	wordAssoDb.connection.on('connected', function () {  
-	  console.log('MONGOOSE default connection OPEN to ' + config.wordAssoDb);
+	wordAssoDb.on("connected", function () {  
+	  console.log("MONGOOSE default connection OPEN to " + config.wordAssoDb);
 	}); 
 
-	wordAssoDb.connection.on('close', function () {  
-	  console.log('MONGOOSE default connection CLOSED to ' + config.wordAssoDb);
+	wordAssoDb.on("close", function () {  
+	  console.log("MONGOOSE default connection CLOSED to " + config.wordAssoDb);
 	}); 
 
-	wordAssoDb.connection.on('error', function (err) {
+	wordAssoDb.on("error", function (err) {
 		console.log("\n\n*** MONGOOSE ERROR ***\n" + err + "\n\n");
 		console.error("\n\n*** MONGOOSE ERROR ***\n" + err + "\n\n");
 		// if (mongoose.connection.readyState == 0) wordAssoDb = mongoose.connect(config.wordAssoDb, options);
 	});
 
 	// When the connection is disconnected
-	wordAssoDb.connection.on('disconnected', function () {  
+	wordAssoDb.on("disconnected", function () {  
 		console.error("\n\n*** MONGOOSE DISCONNECTED ***\n\n");
 	});
 
 
-	require('../app/models/admin.server.model');  
-	require('../app/models/viewer.server.model');  
-	require('../app/models/user.server.model');  
+	require("../app/models/admin.server.model");  
+	require("../app/models/viewer.server.model");  
+	require("../app/models/user.server.model");  
 
-	require('../app/models/group.server.model');  
-	require('../app/models/entity.server.model');  
-	require('../app/models/session.server.model');  
-	require('../app/models/word.server.model');  
-	require('../app/models/tweet.server.model');  
-	require('../app/models/hashtag.server.model');  
-	require('../app/models/media.server.model');  
-	require('../app/models/url.server.model');  
-	require('../app/models/place.server.model');  
-	require('../app/models/phrase.server.model');  
+	require("../app/models/group.server.model");  
+	require("../app/models/entity.server.model");  
+	require("../app/models/session.server.model");  
+	require("../app/models/word.server.model");  
+	require("../app/models/tweet.server.model");  
+	require("../app/models/hashtag.server.model");  
+	require("../app/models/media.server.model");  
+	require("../app/models/url.server.model");  
+	require("../app/models/place.server.model");  
+	require("../app/models/phrase.server.model");  
 
-	require('../app/models/ipAddress.server.model');  
-	require('../app/models/oauth2credential.server.model'); // should probably move admin to separate database
+	require("../app/models/ipAddress.server.model");  
+	require("../app/models/oauth2credential.server.model"); // should probably move admin to separate database
 
 	return wordAssoDb;
 };

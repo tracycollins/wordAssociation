739bd39584f9480e33a6dec7d57a226e798254cf
tracycollins <tc@threeceemedia.com>
2017-08-11 15:53:28 -0400

    update

--- wordAssoServer/config/express.js
+++ wordAssoServer/config/express.js
@@ -1,10 +1,7 @@
 const config = require("./config");
 const express = require("express");
 const bodyParser = require("body-parser");
-	// flash = require("connect-flash"),
 const path = require("path");
-	// session = require("express-session");
-	// MongoStore = require("connect-mongo")(session);
 
 module.exports = function() {
 	const app = express();
@@ -14,14 +11,5 @@ module.exports = function() {
 	}));
 
 	app.use(bodyParser.json());
-
-	// app.set("views", "./app/views");
-	// app.set("view engine", "ejs");
-
-	// app.use(flash());
-
-	// require("../app/routes/admin.server.routes.js")(app);
-	// require("../app/routes/index.server.routes.js")(app);
-
 	return app;
 };

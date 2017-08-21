375ebb9020b4b0c37a42979b5744212554a4055c
tracycollins <tc@threeceemedia.com>
2017-08-21 12:05:03 -0400

    gravity

--- wordAssoServer/js/libs/controlPanel.js
+++ wordAssoServer/js/libs/controlPanel.js
@@ -780,7 +780,7 @@ function ControlPanel() {
       max: 1000.0,
       value: config.defaultGravity,
       // value: config.defaultGravity,
-      multiplier: 1
+      multiplier: 1000.0
     };
 
     var gravitySliderText = {

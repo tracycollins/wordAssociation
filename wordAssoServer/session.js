a9669ec147487142c89e7602415df52a509ea438
tracycollins <tc@threeceemedia.com>
2017-09-20 11:11:29 -0400

    passportSocketIo

--- wordAssoServer/session.js
+++ wordAssoServer/session.js
@@ -824,7 +824,8 @@ function updateLoginButton(){
 
 function login() {
   console.warn("LOGIN: AUTH: " + statsObj.isAuthenticated + " | URL: " + config.authenticationUrl);
-  window.open(config.authenticationUrl, "LOGIN", "_new");
+  // window.open(config.authenticationUrl, "LOGIN", "_new");
+  socket.emit("LOGIN", viewerObj);
   // updateLoginButton();
   // if (controlPanelFlag) {controlPanel.updateControlPanel(config);}
   // saveConfig();

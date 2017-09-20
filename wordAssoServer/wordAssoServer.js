d1d29fa76623057662f0c68e7a0de373aa278271
tracycollins <tc@threeceemedia.com>
2017-09-20 16:53:10 -0400

    log

--- wordAssoServer/wordAssoServer.js
+++ wordAssoServer/wordAssoServer.js
@@ -2561,7 +2561,7 @@ function initAppRouting(callback) {
 
     debug(chalkError("PASSPORT TWITTER AUTH USER\n" + jsonPrint(req.session.passport.user)));  // handle errors
     console.log(chalkError("PASSPORT TWITTER AUTH USER"
-      + " | SID: " + req.session.socket.id
+      + " | SID: " + util.inspect(req.session, {showHidden:false, depth:2})
       + " | @" + req.session.passport.user.screenName
     ));  // handle errors
 

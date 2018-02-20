ea525ec5216ac4bf9abb010a3d1049fedcef9cad
tracycollins <tc@threeceemedia.com>
2018-02-20 02:10:18 -0500

    minRadiusRatio

--- wordAssoServer/js/libs/sessionViewTreepack.js
+++ wordAssoServer/js/libs/sessionViewTreepack.js
@@ -25,7 +25,7 @@ function ViewTreepack() {
   var resumeTimeStamp = 0;
   var compactDateTimeFormat = "YYYYMMDD HHmmss";
 
-  var minRadiusRatio = 0.005;
+  var minRadiusRatio = 0.0075;
   var maxRadiusRatio = 0.10;
 
   var sliderPercision = 5;

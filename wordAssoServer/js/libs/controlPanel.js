d88fc9ea70357d6ea3e23c6632a59d4ab9b9cc84
tracycollins <tc@threeceemedia.com>
2018-05-27 14:09:04 -0400

    more node types

--- wordAssoServer/js/libs/controlPanel.js
+++ wordAssoServer/js/libs/controlPanel.js
@@ -2,8 +2,8 @@
 function ControlPanel() {
 "use strict";
 
-  // var DEFAULT_SOURCE = "http://localhost:9997";
-  var DEFAULT_SOURCE = "http://word.threeceelabs.com";
+  var DEFAULT_SOURCE = "http://localhost:9997";
+  // var DEFAULT_SOURCE = "http://word.threeceelabs.com";
 
   var parentWindow = window.opener;
   console.info("PARENT WINDOW ID | " + parentWindow.PARENT_ID);

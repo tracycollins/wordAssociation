a0fbbe4a29255c207472be3e8d4ad229b4ef0dd1
tracycollins <tc@threeceemedia.com>
2018-06-23 10:45:49 -0400

    control

--- wordAssoServer/js/libs/controlPanel.js
+++ wordAssoServer/js/libs/controlPanel.js
@@ -7,8 +7,8 @@
 function ControlPanel() {
   "use strict";
 
-  var DEFAULT_SOURCE = "http://localhost:9997";
-  // var DEFAULT_SOURCE = "https://word.threeceelabs.com";
+  // var DEFAULT_SOURCE = "http://localhost:9997";
+  var DEFAULT_SOURCE = "https://word.threeceelabs.com";
 
   var parentWindow = window.opener;
   console.info("PARENT WINDOW ID | " + parentWindow.PARENT_ID);

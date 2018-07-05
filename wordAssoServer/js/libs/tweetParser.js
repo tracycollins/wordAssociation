b5b218c923e47004daa814da358254e4770b0712
tracycollins <tc@threeceemedia.com>
2018-07-05 12:00:57 -0400

    hostname

--- wordAssoServer/js/libs/tweetParser.js
+++ wordAssoServer/js/libs/tweetParser.js
@@ -83,6 +83,7 @@ hostname = hostname.replace(/.local/g, "");
 hostname = hostname.replace(/.home/g, "");
 hostname = hostname.replace(/.fios-router.home/g, "");
 hostname = hostname.replace(/word0-instance-1/g, "google");
+hostname = hostname.replace(/word/g, "google");
 
 function jsonPrint(obj) {
   if (obj) {

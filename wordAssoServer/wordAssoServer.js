cb370e62b0184bd8b23426e21b0120c1f4df41ea
tracycollins <tc@threeceemedia.com>
2018-07-05 12:00:57 -0400

    hostname

--- wordAssoServer/wordAssoServer.js
+++ wordAssoServer/wordAssoServer.js
@@ -416,6 +416,7 @@ hostname = hostname.replace(/.at.net/g, "");
 hostname = hostname.replace(/.fios-router/g, "");
 hostname = hostname.replace(/.fios-router.home/g, "");
 hostname = hostname.replace(/word0-instance-1/g, "google");
+hostname = hostname.replace(/word/g, "google");
 
 const chalk = require("chalk");
 const chalkTwitter = chalk.blue;

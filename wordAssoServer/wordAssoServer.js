79cd84fd0c5925374f46f29dce0e0c1a09b24340
tracycollins <tc@threeceemedia.com>
2018-10-23 16:42:27 -0400

    msToTime

--- wordAssoServer/wordAssoServer.js
+++ wordAssoServer/wordAssoServer.js
@@ -1306,17 +1306,25 @@ function printUser(params) {
 }
 
 function msToTime(duration) {
+
+  let sign = 1;
+
+  if (duration < 0) {
+    sign = -1;
+    duration = -duration;
+  }
+
   let seconds = parseInt((duration / 1000) % 60);
   let minutes = parseInt((duration / (1000 * 60)) % 60);
   let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
   let days = parseInt(duration / (1000 * 60 * 60 * 24));
-
   days = (days < 10) ? "0" + days : days;
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-  return days + ":" + hours + ":" + minutes + ":" + seconds;
+  if (sign > 0) return days + ":" + hours + ":" + minutes + ":" + seconds;
+  return "- " + days + ":" + hours + ":" + minutes + ":" + seconds;
 }
 
 let previousSlackMessage = "";

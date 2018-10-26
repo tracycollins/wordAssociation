b3dc904c7195c966365f9cfb4636c577fb465add
tracycollins <tc@threeceemedia.com>
2018-10-25 21:40:13 -0400

    add lots of ;'s to hopefully find uncaught error bug in twit

--- wordAssoServer/js/libs/twit/lib/helpers.js
+++ wordAssoServer/js/libs/twit/lib/helpers.js
@@ -9,13 +9,13 @@ var endpoints = require('./endpoints');
  * @return {String}
  */
 exports.makeQueryString = function (obj) {
-  var qs = querystring.stringify(obj)
+  var qs = querystring.stringify(obj);
   qs = qs.replace(/\!/g, "%21")
          .replace(/\'/g, "%27")
          .replace(/\(/g, "%28")
          .replace(/\)/g, "%29")
          .replace(/\*/g, "%2A");
-  return qs
+  return qs;
 }
 
 /**
@@ -29,20 +29,20 @@ exports.makeQueryString = function (obj) {
  *
  */
 exports.moveParamsIntoPath = function (params, path) {
-  var rgxParam = /\/:(\w+)/g
-  var missingParamErr = null
+  var rgxParam = /\/:(\w+)/g;
+  var missingParamErr = null;
 
   path = path.replace(rgxParam, function (hit) {
-    var paramName = hit.slice(2)
-    var suppliedVal = params[paramName]
+    var paramName = hit.slice(2);
+    var suppliedVal = params[paramName];
     if (!suppliedVal) {
-      throw new Error('Twit: Params object is missing a required parameter for this request: `'+paramName+'`')
+      throw new Error('Twit: Params object is missing a required parameter for this request: `'+paramName+'`');
     }
-    var retVal = '/' + suppliedVal
-    delete params[paramName]
-    return retVal
+    var retVal = '/' + suppliedVal;
+    delete params[paramName];
+    return retVal;
   })
-  return path
+  return path;
 }
 
 /**
@@ -56,29 +56,29 @@ exports.moveParamsIntoPath = function (params, path) {
 exports.attachBodyInfoToError = function (err, body) {
   err.twitterReply = body;
   if (!body) {
-    return
+    return;
   }
   if (body.error) {
     // the body itself is an error object
-    err.message = body.error
-    err.allErrors = err.allErrors.concat([body])
+    err.message = body.error;
+    err.allErrors = err.allErrors.concat([body]);
   } else if (body.errors && body.errors.length) {
     // body contains multiple error objects
     err.message = body.errors[0].message;
     err.code = body.errors[0].code;
-    err.allErrors = err.allErrors.concat(body.errors)
+    err.allErrors = err.allErrors.concat(body.errors);
   }
 }
 
 exports.makeTwitError = function (message) {
-  var err = new Error()
+  var err = new Error();
   if (message) {
-    err.message = message
+    err.message = message;
   }
-  err.code = null
-  err.allErrors = []
-  err.twitterReply = null
-  return err
+  err.code = null;
+  err.allErrors = [];
+  err.twitterReply = null;
+  return err;
 }
 
 /**
@@ -103,7 +103,7 @@ exports.getBearerToken = function (consumer_key, consumer_secret, cb) {
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     },
     body: 'grant_type=client_credentials',
-    json: true,
+    json: true
   }, function (err, res, body) {
     if (err) {
       var error = exports.makeTwitError(err.toString());

d2cfe067b521cad207e8fe1384d7a2a7555e7ff7
threecee <tc@threeceemedia.com>
2019-01-14 20:07:20 -0500

    Update sessionViewTreepack.js

--- wordAssoServer/js/libs/sessionViewTreepack.js
+++ wordAssoServer/js/libs/sessionViewTreepack.js
@@ -504,7 +504,7 @@ function ViewTreepack() {
     {
       maxZoom: 2, 
       minZoom: 0.1,
-      zoomSpeed: 0.01
+      zoomSpeed: 0.02
     }
   )
   .zoomAbs(0.5*width, 0.5*height, zoomFactor);

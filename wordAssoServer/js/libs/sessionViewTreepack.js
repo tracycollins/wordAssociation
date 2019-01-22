2c712166a7db08e146395cc302440288dc7f3646
tracycollins <tc@threeceemedia.com>
2019-01-21 20:39:52 -0500

    Update sessionViewTreepack.js

--- wordAssoServer/js/libs/sessionViewTreepack.js
+++ wordAssoServer/js/libs/sessionViewTreepack.js
@@ -1439,6 +1439,10 @@ function ViewTreepack() {
           if (!d.category && !d.categoryAuto) { return palette.black; }
           return d.categoryColor; 
         })
+        .style("fill-opacity", function(d) { 
+          if (d.isTopTerm) { return nodeLabelOpacityScaleTopTerm(d.ageMaxRatio); }
+          return nodeLabelOpacityScale(d.ageMaxRatio); 
+        })
         .style("stroke", function (d) {
           if (d.categoryMismatch) { return palette.red; }
           if (d.categoryMatch) { return categoryMatchColor; }
@@ -1459,6 +1463,10 @@ function ViewTreepack() {
           if (d.categoryAuto && !d.following) { return 0.5*categoryAutoStrokeWidth; }
           if (d.following) { return defaultStrokeWidth; }
           return 0.5*defaultStrokeWidth; 
+        })
+        .style("stroke-opacity", function(d) { 
+          if (d.isTopTerm) { return nodeLabelOpacityScaleTopTerm(d.ageMaxRatio); }
+          return nodeLabelOpacityScale(d.ageMaxRatio); 
         });
     }
   };
@@ -1542,46 +1550,46 @@ function ViewTreepack() {
       })
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
-      .style("fill", function (d) { 
-        if (d.isTopTerm && !d.category && !d.categoryAuto) { return palette.white; }
-        if (!d.category && !d.categoryAuto) { return palette.black; }
-        if (d.category) { return d.categoryColor; }
-        if (d.categoryAuto === "right") { return palette.yellow; }
-        if (d.categoryAuto === "left") { return palette.blue; }
-        if (d.categoryAuto === "positive") { return palette.green; }
-        if (d.categoryAuto ==="negative") { return palette.red; }
-        return d.categoryColor; 
-      })
-      .style("stroke", function (d) {
-        if (d.categoryMismatch) { return palette.red; }
-        if (d.categoryMatch) { return categoryMatchColor; }
-        if (d.categoryAuto === "right") { return palette.yellow; }
-        if (d.categoryAuto === "left") { return palette.blue; }
-        if (d.categoryAuto === "positive") { return palette.green; }
-        if (d.categoryAuto ==="negative") { return palette.black; }
-        return palette.white; 
-      })
-      .style("stroke-width", function (d) { 
-        if (d.categoryMismatch && d.following) { return categoryMismatchStrokeWidth; }
-        if (d.categoryMismatch && !d.following) { return 0.5*categoryMismatchStrokeWidth; }
-        if (d.categoryMatch && d.following) { return categoryMatchStrokeWidth; }
-        if (d.categoryMatch && !d.following) { return 0.5*categoryMatchStrokeWidth; }
-        if (d.isTopTerm && d.following) { return topTermStrokeWidth; }
-        if (d.isTopTerm && !d.following) { return 0.5*topTermStrokeWidth; }
-        if (d.categoryAuto && d.following) { return categoryAutoStrokeWidth; }
-        if (d.categoryAuto && !d.following) { return 0.5*categoryAutoStrokeWidth; }
-        if (d.following) { return defaultStrokeWidth; }
-        return 0.5*defaultStrokeWidth; 
-      })
-      .style("fill-opacity", function(d) { 
-        if (d.isTopTerm) { return nodeLabelOpacityScaleTopTerm(d.ageMaxRatio); }
-        return nodeLabelOpacityScale(d.ageMaxRatio); 
-      })
-      .style("stroke-opacity", function(d) { 
-        if (d.isTopTerm) { return nodeLabelOpacityScaleTopTerm(d.ageMaxRatio); }
-        return nodeLabelOpacityScale(d.ageMaxRatio); 
-      });
-      // .each(updateChangedCircleNodes);
+      // .style("fill", function (d) { 
+      //   if (d.isTopTerm && !d.category && !d.categoryAuto) { return palette.white; }
+      //   if (!d.category && !d.categoryAuto) { return palette.black; }
+      //   if (d.category) { return d.categoryColor; }
+      //   if (d.categoryAuto === "right") { return palette.yellow; }
+      //   if (d.categoryAuto === "left") { return palette.blue; }
+      //   if (d.categoryAuto === "positive") { return palette.green; }
+      //   if (d.categoryAuto ==="negative") { return palette.red; }
+      //   return d.categoryColor; 
+      // })
+      // .style("stroke", function (d) {
+      //   if (d.categoryMismatch) { return palette.red; }
+      //   if (d.categoryMatch) { return categoryMatchColor; }
+      //   if (d.categoryAuto === "right") { return palette.yellow; }
+      //   if (d.categoryAuto === "left") { return palette.blue; }
+      //   if (d.categoryAuto === "positive") { return palette.green; }
+      //   if (d.categoryAuto ==="negative") { return palette.black; }
+      //   return palette.white; 
+      // })
+      // .style("stroke-width", function (d) { 
+      //   if (d.categoryMismatch && d.following) { return categoryMismatchStrokeWidth; }
+      //   if (d.categoryMismatch && !d.following) { return 0.5*categoryMismatchStrokeWidth; }
+      //   if (d.categoryMatch && d.following) { return categoryMatchStrokeWidth; }
+      //   if (d.categoryMatch && !d.following) { return 0.5*categoryMatchStrokeWidth; }
+      //   if (d.isTopTerm && d.following) { return topTermStrokeWidth; }
+      //   if (d.isTopTerm && !d.following) { return 0.5*topTermStrokeWidth; }
+      //   if (d.categoryAuto && d.following) { return categoryAutoStrokeWidth; }
+      //   if (d.categoryAuto && !d.following) { return 0.5*categoryAutoStrokeWidth; }
+      //   if (d.following) { return defaultStrokeWidth; }
+      //   return 0.5*defaultStrokeWidth; 
+      // })
+      // .style("fill-opacity", function(d) { 
+      //   if (d.isTopTerm) { return nodeLabelOpacityScaleTopTerm(d.ageMaxRatio); }
+      //   return nodeLabelOpacityScale(d.ageMaxRatio); 
+      // })
+      // .style("stroke-opacity", function(d) { 
+      //   if (d.isTopTerm) { return nodeLabelOpacityScaleTopTerm(d.ageMaxRatio); }
+      //   return nodeLabelOpacityScale(d.ageMaxRatio); 
+      // })
+      .each(updateChangedCircleNodes);
 
     // EXIT
     nodeCircles

32ca6bd86f71cd9101e8b3f98d375038679cc8b9
threecee <tc@threeceemedia.com>
2019-05-21 22:15:03 -0400

    Update wordAssoServer.js

--- wordAssoServer/wordAssoServer.js
+++ wordAssoServer/wordAssoServer.js
@@ -7196,7 +7196,7 @@ function initAppRouting(callback) {
         + " | HOST: " + req.hostname
         + " | METHOD: " + req.method
         + " | PATH: " + req.path
-        + "\nreq.body\n" + jsonPrint(req.body)
+        + "\nreq.body\n" + jsonPrint(JSONParse(req.body))
       )); 
 
       res.sendStatus(200);

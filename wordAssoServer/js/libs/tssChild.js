10e3d0824247ca365132ae35cb30e1a226df1f5f
threecee <tc@threeceemedia.com>
2019-10-15 22:43:12 -0400

    update

--- wordAssoServer/js/libs/tssChild.js
+++ wordAssoServer/js/libs/tssChild.js
@@ -61,6 +61,7 @@ hostname = hostname.replace(/.fios-router/g, "");
 hostname = hostname.replace(/.fios-router.home/g, "");
 hostname = hostname.replace(/word0-instance-1/g, "google");
 hostname = hostname.replace(/word/g, "google");
+hostname = hostname.replace(/word-1/g, "google");
 
 const _ = require("lodash");
 const async = require("async");

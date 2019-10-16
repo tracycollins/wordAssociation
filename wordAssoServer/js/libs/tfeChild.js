dbc3fd8fef496fb3c6145a99f5e070bc5b4a80da
threecee <tc@threeceemedia.com>
2019-10-15 23:02:56 -0400

    hostname

--- wordAssoServer/js/libs/tfeChild.js
+++ wordAssoServer/js/libs/tfeChild.js
@@ -74,8 +74,8 @@ hostname = hostname.replace(/.at.net/g, "");
 hostname = hostname.replace(/.fios-router/g, "");
 hostname = hostname.replace(/.fios-router.home/g, "");
 hostname = hostname.replace(/word0-instance-1/g, "google");
-hostname = hostname.replace(/word/g, "google");
 hostname = hostname.replace(/word-1/g, "google");
+hostname = hostname.replace(/word/g, "google");
 
 let DROPBOX_ROOT_FOLDER;
 

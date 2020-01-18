d40603db7b4695116cb8a080ae35264e7c8f01b1
threecee <tc@threeceemedia.com>
2020-01-18 15:33:59 -0500

    Update session.js

--- wordAssoServer/session.js
+++ wordAssoServer/session.js
@@ -1950,7 +1950,8 @@ function initSocketSessionUpdateRx(){
     viewNodeAddQlength = currentSessionView.getNodeAddQlength();
     viewNumNodes = currentSessionView.getNumNodes();
 
-    if (rxNodeQueueReady && (rxNodeQueue.length > 0) && (viewNumNodes <= 1.5*DEFAULT_MAX_NODES) && (viewNodeAddQlength <= 1.5*RX_NODE_QUEUE_MAX)) {
+    // if (rxNodeQueueReady && (rxNodeQueue.length > 0) && (viewNumNodes <= 1.5*DEFAULT_MAX_NODES) && (viewNodeAddQlength <= 1.5*RX_NODE_QUEUE_MAX)) {
+    if (rxNodeQueueReady && (rxNodeQueue.length > 0) && (viewNodeAddQlength <= 1.5*RX_NODE_QUEUE_MAX)) {
 
       rxNodeQueueReady = false;
 

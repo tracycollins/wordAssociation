df134eda71e984ec24949cd6966137991817b622
tracycollins <tc@threeceemedia.com>
2020-11-15 16:11:05 -0500

    package

--- wordAssoServer/categorizer/src/User.js
+++ wordAssoServer/categorizer/src/User.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Timeline } from 'react-twitter-widgets'
 
 import Duration from 'duration';
-import CircularProgress from '@material-ui/core/CircularProgress';
+// import CircularProgress from '@material-ui/core/CircularProgress';
 
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
@@ -11,7 +11,7 @@ import Card from '@material-ui/core/Card';
 // import CardActionArea from '@material-ui/core/CardActionArea';
 // import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
+// import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 // import CheckBoxIcon from '@material-ui/icons/CheckBox';
 // import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';

709cb2d5c6214bed70f76bf162a01d8f84097bdd
tracycollins <tc@threeceemedia.com>
2020-11-18 13:35:27 -0500

    package

--- wordAssoServer/categorizer/src/index.js
+++ wordAssoServer/categorizer/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
-  Switch,
-  useLocation
 } from "react-router-dom";
 import './index.css';
 import App from './App';

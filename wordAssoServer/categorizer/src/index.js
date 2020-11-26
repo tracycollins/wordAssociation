e6a178d4b9dca8444b48e8bcdb27bddf1d6cb682
tracycollins <tc@threeceemedia.com>
2020-11-26 13:32:44 -0500

    package

--- wordAssoServer/categorizer/src/index.js
+++ wordAssoServer/categorizer/src/index.js
@@ -2,17 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
+  Switch,
+  Route,
 } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render(
+//   <Router>
+//     <div>
+//       <Switch>
+//         <Route path="/categorize/user/:slug">
+//           <App />
+//         </Route>
+//       </Switch>
+//     </div>
+//   </Router>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
   <Router>
-    <App />
+    <div>
+      <Switch>
+        <Route path="/categorize/user/:slug">
+          <App />
+        </Route>
+        <Route path="/categorize/hashtag/:slug">
+          <App />
+        </Route>
+        <Route >
+          <App />
+        </Route>
+      </Switch>
+    </div>
   </Router>,
   document.getElementById('root')
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

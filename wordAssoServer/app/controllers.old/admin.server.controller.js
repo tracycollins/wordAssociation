10fdf09632b051e2be3fa4aeecc1212bc260bb59
tracycollins <tc@threeceemedia.com>
2020-12-24 01:49:49 -0500

    package

--- wordAssoServer/app/controllers.old/admin.server.controller.js
+++
@@ -1,5 +0,0 @@
-exports.render = function(req, res) {
-    res.render('admin', {
-    	title: 'blackLivesMatter ADMIN'
-    });
-};
--- wordAssoServer/app/controllers.old/index.server.controller.js
+++
@@ -1,6 +0,0 @@
-exports.render = function(req, res) {
-    res.render('index', {
-    	title: 'blackLivesMatter',
-    	tweet: req.tweet ? req.tweet.tweetId : ''
-    });
-};

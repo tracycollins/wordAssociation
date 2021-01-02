424ddb8676789ebcc81b57d8c13007324eede09c
tracycollins <tc@threeceemedia.com>
2021-01-02 17:33:40 -0500

    package

--- wordAssoServer/customizer/src/config.js
+++ wordAssoServer/customizer/src/config.js
@@ -1,30 +1,30 @@
 const config = {
   defaults: {
-    maxNodesLimit: 47,
+    maxNodesLimit: 100,
     maxNodesLimitRange: {
       min: 0,
-      max: 100,
+      max: 200,
       step: 1
     },
-    nodeMaxAge: 10000,
+    nodeMaxAge: 20000,
     nodeMaxAgeRange: {
       min: 0,
       max: 60000,
       step: 100
     },
-    velocityDecay: 0.5,
+    velocityDecay: 0.8,
     velocityDecayRange: {
       min: 0.0,
       max: 1.0,
       step: 0.01
     },
-    charge: -50,
+    charge: -500,
     chargeRange: {
       min: -1000,
       max: 1000,
       step: 10
     },
-    gravity: 0.001,
+    gravity: 0.005,
     gravityRange: {
       min: -0.002,
       max: 0.002,
@@ -45,8 +45,8 @@ const config = {
       step: 0.001
     },
     fontSizeRatio: {
-      min: 0.047,
-      max: 0.47
+      min: 0.015,
+      max: 0.45
     }
   },
   settings: {
@@ -76,18 +76,18 @@ const config = {
     },
     gravity: 0.001,
     gravityRange: {
-      min: -0.002,
-      max: 0.002,
+      min: -0.0001,
+      max: 0.01,
       step: 0.00001
     },
     nodeRadiusRatioRange: {
       min: 0.0,
-      max: 1.0,
+      max: 0.5,
       step: 0.001
     },
     nodeRadiusRatio: {
-      min: 0.047,
-      max: 0.47
+      min: 0.015,
+      max: 0.08
     },
     fontSizeRatioRange: {
       min: 0.0,

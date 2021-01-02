9e28feef8c338dc6ee3524376922afed9435bc09
tracycollins <tc@threeceemedia.com>
2021-01-02 17:39:08 -0500

    package

--- wordAssoServer/customizer/src/SettingsView.js
+++ wordAssoServer/customizer/src/SettingsView.js
@@ -33,7 +33,10 @@ const Settings = (props) => {
           <Grid item className={classes.gridItem} xs={3}>
 
             <Typography className={classes.range} id="nodeRadiusRatio" name="nodeRadiusRatio" gutterBottom>
-              {`NODE RADIUS RATIO ${props.settings.nodeRadiusRatio.min} min/ ${props.settings.nodeRadiusRatio.max} max`}
+              NODE RADIUS RATIO
+            </Typography>
+            <Typography gutterBottom>
+              {`${props.settings.nodeRadiusRatio.min} min/ ${props.settings.nodeRadiusRatio.max} max`}
             </Typography>
             <Slider
               id="nodeRadiusRatio"

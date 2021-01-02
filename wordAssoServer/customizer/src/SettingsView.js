f2fb7c2a977d94df9643ef85eead49f16991448a
tracycollins <tc@threeceemedia.com>
2021-01-02 17:33:40 -0500

    package

--- wordAssoServer/customizer/src/SettingsView.js
+++ wordAssoServer/customizer/src/SettingsView.js
@@ -1,45 +1,6 @@
-import React, { useState } from 'react';
-// import React from 'react';
-// import { green } from '@material-ui/core/colors';
-// import clsx from 'clsx';
-
-// import Duration from 'duration';
-
-// import AppBar from '@material-ui/core/AppBar';
-// import Button from '@material-ui/core/Button';
-// import ButtonGroup from '@material-ui/core/ButtonGroup';
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import CheckBoxIcon from '@material-ui/icons/CheckBox';
-// import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import FormControl from '@material-ui/core/FormControl';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import FormGroup from '@material-ui/core/FormGroup';
-// import FormLabel from '@material-ui/core/FormLabel';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
-// import IconButton from '@material-ui/core/IconButton';
-// import InputBase from '@material-ui/core/InputBase';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import Paper from '@material-ui/core/Paper';
-// import Radio from '@material-ui/core/Radio';
-// import RadioGroup from '@material-ui/core/RadioGroup';
-// import SearchIcon from '@material-ui/icons/Search';
-// import Select from '@material-ui/core/Select';
 import Slider from '@material-ui/core/Slider';
-// import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
-// import TableCell from '@material-ui/core/TableCell';
-// import TableContainer from '@material-ui/core/TableContainer';
-// import TableHead from '@material-ui/core/TableHead';
-// import TableRow from '@material-ui/core/TableRow';
-// import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -61,32 +22,8 @@ const Settings = (props) => {
   
   const classes = useStyles();
 
-  // const defaultSettingsState = {
-  //   nodeRadiusRatio : [
-  //     props.settings.nodeRadiusRatio.min, 
-  //     props.settings.nodeRadiusRatio.max
-  //   ],
-  //   fontSizeRatio : [
-  //     props.settings.fontSizeRatio.min, 
-  //     props.settings.fontSizeRatio.max
-  //   ],
-  //   metricMode: "rate",
-  //   ageNodes: true,
-  //   autoCategoryFlag: false,
-  //   testMode: false,
-  //   pause: false,
-  //   charge: props.settings.charge,
-  //   gravity: props.settings.gravity,
-  //   velocityDecay: props.settings.velocityDecay,
-  //   maxNodesLimit: props.settings.maxNodesLimit,
-  //   nodeMaxAge: props.settings.nodeMaxAge,
-  // }
-
-  // const [settings, setSettings] = useState(defaultSettingsState);
-
   const handleChangeSettings = name => (e, value) => {
     e.preventDefault();
-    // setSettings(prevState => ({...prevState, [name]: value}));
     props.handleChange({name: name, value: value})
   };
 
@@ -96,7 +33,7 @@ const Settings = (props) => {
           <Grid item className={classes.gridItem} xs={3}>
 
             <Typography className={classes.range} id="nodeRadiusRatio" name="nodeRadiusRatio" gutterBottom>
-              NODE RADIUS RATIO min/max
+              {`NODE RADIUS RATIO ${props.defaults.nodeRadiusRatioRange.min} min/ ${props.defaults.nodeRadiusRatioRange.max} max`}
             </Typography>
             <Slider
               id="nodeRadiusRatio"

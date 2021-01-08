e0e0616abdab27f078bdcb6945e7055b876d1844
tracycollins <tc@threeceemedia.com>
2021-01-08 04:22:43 -0500

    package

--- wordAssoServer/customizer/src/StatsView.js
+++ wordAssoServer/customizer/src/StatsView.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
 const Stats = (props) => {
 
   // const { heartbeat } = props;
-  // console.log(props)
+  console.log(props)
   
   // statsObj.nodesPerSec = 0.0;
   // statsObj.nodesPerMin = 0.0;

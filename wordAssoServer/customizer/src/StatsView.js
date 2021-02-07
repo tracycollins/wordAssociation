9c1dd6a2504b0ba6f692c05b57cabf746662ab03
tracycollins <tc@threeceemedia.com>
2021-02-07 14:45:55 -0500

    package

--- wordAssoServer/customizer/src/StatsView.js
+++ wordAssoServer/customizer/src/StatsView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // import clsx from 'clsx';
-import { green } from '@material-ui/core/colors';
+import { grey } from '@material-ui/core/colors';
 
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
@@ -17,6 +17,9 @@ import Typography from '@material-ui/core/Typography';
 // import TableRow from '@material-ui/core/TableRow';
 
 import { makeStyles } from '@material-ui/core/styles';
+
+const lightGray = '#202020';
+const textLightGray = '#CCCCCC';
 
 // const StyledTableCell = withStyles((theme) => ({
 //   head: {
@@ -35,98 +38,48 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     border: 0,
+    margin: 0,
+    padding: 0,
     flexGrow: 2,
   },
-  appBar: {
-    border: 0,
-    backgroundColor: 'white',
-    marginBottom: theme.spacing(1),
+  multilineColor:{
+    color: textLightGray
   },
   grid: {
     border: 0,
     display: 'flex',
-    alignItems: 'stretch',
+    // alignItems: 'stretch',
+  },
+  gridSetting: {
+    // border: 0,
+    flexFlow: 'column',
+    display: 'flex',
+    // margin: theme.spacing(1),
+    // alignItems: 'flexStart',
   },
   gridItem: {
+    background: lightGray,
     border: 0,
     margin: theme.spacing(1),
+    padding: theme.spacing(4),
   },
   gridHeader: {
-    padding: theme.spacing(1),
     border: 0,
-    marginBottom: theme.spacing(1),
   },
   paper: {
-    padding: theme.spacing(1),
+    color: textLightGray,
+    backgroundColor: lightGray,
+    // padding: theme.spacing(1),
     outlined: true,
     variant: 'outlined',
   },
-  timelineError: {
-    textAlign: "center",
-    border: '2px solid red',
-    color: 'red',
-    backgroundColor: 'white',
-  },
-  card: {
-    alignSelf: "center",
-  },
-  profileImage: {
-    marginBottom: theme.spacing(1),
-  },
-  bannerImage: {
-    marginBottom: theme.spacing(1),
-  },
-  icon: {
-    borderRadius: '50%',
-    width: 16,
-    height: 16,
-    boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
-    backgroundColor: '#f5f8fa',
-    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
-    '$root.Mui-focusVisible &': {
-      outline: '2px auto rgba(19,124,189,.6)',
-      outlineOffset: 2,
-    },
-    'input:hover ~ &': {
-      backgroundColor: '#ebf1f5',
-    },
-    'input:disabled ~ &': {
-      boxShadow: 'none',
-      background: 'rgba(206,217,224,.5)',
-    },
-  },
-  checkedIcon: {
-    backgroundColor: '#137cbd',
-    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
-    '&:before': {
-      display: 'block',
-      width: 16,
-      height: 16,
-      backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
-      content: '""',
-    },
-    'input:hover ~ &': {
-      backgroundColor: '#106ba3',
-    },
-  },
-  selectCategory: {
-    border: '1px solid darkgray',
-    borderRadius: theme.shape.borderRadius,
-    fontSize: '0.9rem',
-    padding: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
   radioGroupCategory: {
-    maxWidth: "90%",
+    maxWidth: "80%",
     fontSize: '0.5rem',
-    padding: theme.spacing(2),
-    marginBottom: theme.spacing(1),
   },
   checkbox: {
-    color: green[400],
-    '&$checked': {
-      color: green[600],
-    },
+    color: grey[400],
+    '&$checked': { color: grey[600],},
   },
   checked: {},
   radioButtonLabel: {
@@ -134,129 +87,266 @@ const useStyles = makeStyles((theme) => ({
   },
   radioButton: {
   },
-  table: {
-    maxWidth: "95%",
-    align: 'center',
-    padding: theme.spacing(1),
-  },
-  tableHead: {
-    backgroundColor: '#ddeeee',
-  },
-  tableCell: {
-  },
-  tableCategorized: {
-    backgroundColor: '#ddeeee',
-  },
-  tableRowGreen: {
-    backgroundColor: 'lightgreen',
-  },
-  statusBar: {
-    raised: false,
-    backgroundColor: 'white',
-    margin: 2,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    color: 'blue',
-  },
-  search: {
-    position: 'relative',
+  range: {
+    // width: 'fit-content',
+    border: `1px solid ${theme.palette.divider}`,
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: "white",
-    '&:hover': {
-      backgroundColor: "#ddeeee",
-    },
-    marginRight: theme.spacing(1),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 'auto',
-    },
+    // backgroundColor: theme.palette.background.paper,
+    // color: theme.palette.text.secondary,
+    color: textLightGray,
   },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+  slider: {
+    // margin: theme.spacing(1),
   },
-  inputRoot: {
-    color: 'primary',
+  sectionLabel: {
+    fontSize: '1.4rem',
+    fontWeight: '400',
+    color: textLightGray,
   },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
+  settingLabel: {
+    fontSize: '0.8rem',
+    // margin: theme.spacing(1),
+    color: textLightGray,
   },
-
-  buttonGroupLabel: {
-    color: 'blue',
-    marginRight: theme.spacing(1),
+  settingValue: {
+    fontSize: '1.5rem',
+    // margin: theme.spacing(1),
+    color: textLightGray,
   },
-  buttonAll: {
-    color: 'black',
+  textField: {
+    color: textLightGray,
+    '& input:valid + fieldset': {
+      borderColor: textLightGray,
+      // borderWidth: 2,
+    }
   },
-  buttonLeft: {
-    color: 'blue',
-  },
-  buttonNeutral: {
-    color: 'gray',
-  },
-  buttonRight: {
-    color: 'red',
-  },
-  buttonMismatch: {
-    margin: 5
-  },
-  autoCategory:{
-    borderRadius: theme.shape.borderRadius,
-    padding: theme.spacing(1),
-    color: 'white',
-    marginBottom: theme.spacing(1),
-  },
-  category: {
-    borderRadius: theme.shape.borderRadius,
-    padding: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-
-  left: {
-    backgroundColor: 'blue',
-    color: 'white',
-  },
-  neutral: {
-    backgroundColor: 'darkgray',
-    color: 'white',
-  },
-  right: {
-    backgroundColor: 'red',
-    color: 'white',
-  },
-  positive: {
-    backgroundColor: 'green',
-    color: 'white',
-  },
-  negative: {
-    backgroundColor: 'yellow',
-    color: 'black',
-  },
-  none: {
-    backgroundColor: 'white',
-    color: 'black',
-  },
-  ignored: {
-    backgroundColor: 'yellow',
-    color: 'black',
-  },
-
 }));
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     border: 0,
+//     flexGrow: 2,
+//   },
+//   appBar: {
+//     border: 0,
+//     backgroundColor: 'white',
+//     marginBottom: theme.spacing(1),
+//   },
+//   grid: {
+//     border: 0,
+//     display: 'flex',
+//     alignItems: 'stretch',
+//   },
+//   gridItem: {
+//     border: 0,
+//     margin: theme.spacing(1),
+//   },
+//   gridHeader: {
+//     padding: theme.spacing(1),
+//     border: 0,
+//     marginBottom: theme.spacing(1),
+//   },
+//   paper: {
+//     padding: theme.spacing(1),
+//     outlined: true,
+//     variant: 'outlined',
+//   },
+//   timelineError: {
+//     textAlign: "center",
+//     border: '2px solid red',
+//     color: 'red',
+//     backgroundColor: 'white',
+//   },
+//   card: {
+//     alignSelf: "center",
+//   },
+//   profileImage: {
+//     marginBottom: theme.spacing(1),
+//   },
+//   bannerImage: {
+//     marginBottom: theme.spacing(1),
+//   },
+//   icon: {
+//     borderRadius: '50%',
+//     width: 16,
+//     height: 16,
+//     boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+//     backgroundColor: '#f5f8fa',
+//     backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+//     '$root.Mui-focusVisible &': {
+//       outline: '2px auto rgba(19,124,189,.6)',
+//       outlineOffset: 2,
+//     },
+//     'input:hover ~ &': {
+//       backgroundColor: '#ebf1f5',
+//     },
+//     'input:disabled ~ &': {
+//       boxShadow: 'none',
+//       background: 'rgba(206,217,224,.5)',
+//     },
+//   },
+//   checkedIcon: {
+//     backgroundColor: '#137cbd',
+//     backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+//     '&:before': {
+//       display: 'block',
+//       width: 16,
+//       height: 16,
+//       backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
+//       content: '""',
+//     },
+//     'input:hover ~ &': {
+//       backgroundColor: '#106ba3',
+//     },
+//   },
+//   selectCategory: {
+//     border: '1px solid darkgray',
+//     borderRadius: theme.shape.borderRadius,
+//     fontSize: '0.9rem',
+//     padding: theme.spacing(1),
+//     marginBottom: theme.spacing(1),
+//   },
+//   radioGroupCategory: {
+//     maxWidth: "90%",
+//     fontSize: '0.5rem',
+//     padding: theme.spacing(2),
+//     marginBottom: theme.spacing(1),
+//   },
+//   checkbox: {
+//     color: green[400],
+//     '&$checked': {
+//       color: green[600],
+//     },
+//   },
+//   checked: {},
+//   radioButtonLabel: {
+//     fontSize: '0.9rem'
+//   },
+//   radioButton: {
+//   },
+//   table: {
+//     maxWidth: "95%",
+//     align: 'center',
+//     padding: theme.spacing(1),
+//   },
+//   tableHead: {
+//     backgroundColor: '#ddeeee',
+//   },
+//   tableCell: {
+//   },
+//   tableCategorized: {
+//     backgroundColor: '#ddeeee',
+//   },
+//   tableRowGreen: {
+//     backgroundColor: 'lightgreen',
+//   },
+//   statusBar: {
+//     raised: false,
+//     backgroundColor: 'white',
+//     margin: 2,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     color: 'blue',
+//   },
+//   search: {
+//     position: 'relative',
+//     borderRadius: theme.shape.borderRadius,
+//     backgroundColor: "white",
+//     '&:hover': {
+//       backgroundColor: "#ddeeee",
+//     },
+//     marginRight: theme.spacing(1),
+//     width: '100%',
+//     [theme.breakpoints.up('sm')]: {
+//       width: 'auto',
+//     },
+//   },
+//   searchIcon: {
+//     padding: theme.spacing(0, 2),
+//     height: '100%',
+//     position: 'absolute',
+//     pointerEvents: 'none',
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   inputRoot: {
+//     color: 'primary',
+//   },
+//   inputInput: {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+//     transition: theme.transitions.create('width'),
+//     width: '100%',
+//     [theme.breakpoints.up('md')]: {
+//       width: '20ch',
+//     },
+//   },
+
+//   buttonGroupLabel: {
+//     color: 'blue',
+//     marginRight: theme.spacing(1),
+//   },
+//   buttonAll: {
+//     color: 'black',
+//   },
+//   buttonLeft: {
+//     color: 'blue',
+//   },
+//   buttonNeutral: {
+//     color: 'gray',
+//   },
+//   buttonRight: {
+//     color: 'red',
+//   },
+//   buttonMismatch: {
+//     margin: 5
+//   },
+//   autoCategory:{
+//     borderRadius: theme.shape.borderRadius,
+//     padding: theme.spacing(1),
+//     color: 'white',
+//     marginBottom: theme.spacing(1),
+//   },
+//   category: {
+//     borderRadius: theme.shape.borderRadius,
+//     padding: theme.spacing(1),
+//     marginBottom: theme.spacing(1),
+//   },
+
+//   left: {
+//     backgroundColor: 'blue',
+//     color: 'white',
+//   },
+//   neutral: {
+//     backgroundColor: 'darkgray',
+//     color: 'white',
+//   },
+//   right: {
+//     backgroundColor: 'red',
+//     color: 'white',
+//   },
+//   positive: {
+//     backgroundColor: 'green',
+//     color: 'white',
+//   },
+//   negative: {
+//     backgroundColor: 'yellow',
+//     color: 'black',
+//   },
+//   none: {
+//     backgroundColor: 'white',
+//     color: 'black',
+//   },
+//   ignored: {
+//     backgroundColor: 'yellow',
+//     color: 'black',
+//   },
+
+// }));
 
 
 function LinearProgressWithLabel(props) {
@@ -293,7 +383,7 @@ const Stats = (props) => {
     <>
       <Grid className={classes.grid}>
           <Grid item className={classes.gridItem} xs={4}>
-            <Card className={classes.card} variant="outlined">
+            {/* <Card className={classes.card} variant="outlined">
               <CardContent >
                 <Typography variant="h6" id="neuralNetworks" name="neuralNetworks" gutterBottom>
                   BEST NEURAL NETWORK
@@ -308,7 +398,49 @@ const Stats = (props) => {
                     <span><b>{props.heartbeat.bestNetwork.successRate.toFixed(2)}</b>% SUCCESS RATE</span>
                   </Typography>
               </CardContent>              
-            </Card>
+            </Card> */}
+            <Grid className={classes.gridSetting}>
+              <Typography className={classes.sectionLabel} gutterBottom>
+                BEST NEURAL NETWORK
+              </Typography>
+              <Typography className={classes.settingLabel}>
+                NETWORK ID
+              </Typography>
+              <Grid item xs={8}>
+                <Typography className={classes.settingValue} gutterBottom>
+                  {props.heartbeat.bestNetwork.networkId || '---'}
+                </Typography>
+              </Grid>
+              <Typography className={classes.settingLabel}>
+                INPUTS ID
+              </Typography>
+              <Grid item xs={8}>
+                <Typography className={classes.settingValue} gutterBottom>
+                  {props.heartbeat.bestNetwork.inputsId || '---'}
+                </Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <Typography className={classes.settingLabel}>
+                  LIVE RATE
+                </Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <Typography className={classes.settingValue} gutterBottom>
+                  {`${props.heartbeat.bestNetwork.runtimeMatchRate.toFixed(2)}%`}
+                </Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <Typography className={classes.settingLabel}>
+                  SUCCESS RATE
+                </Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <Typography className={classes.settingValue} gutterBottom>
+                  {`${props.heartbeat.bestNetwork.successRate.toFixed(2)}%`}
+                </Typography>
+              </Grid>
+            </Grid>
+
           </Grid>
           <Grid item className={classes.gridItem} xs={4}>
             <Card className={classes.card} variant="outlined">

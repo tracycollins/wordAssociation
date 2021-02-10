1a777c2d2e07b551ee969c6178756809c0f64ef9
tracycollins <tc@threeceemedia.com>
2021-02-10 02:17:27 -0500

    package

--- wordAssoServer/categorizer/src/HashtagView.js
+++ wordAssoServer/categorizer/src/HashtagView.js
@@ -5,41 +5,21 @@ import { Tweet } from 'react-twitter-widgets'
 import Duration from 'duration';
 import { green } from '@material-ui/core/colors';
 
-// import AppBar from '@material-ui/core/AppBar';
-// // import Box from '@material-ui/core/Box';
-// import Button from '@material-ui/core/Button';
-// import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-// import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import CheckBoxIcon from '@material-ui/icons/CheckBox';
-// import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
-// import FormLabel from '@material-ui/core/FormLabel';
 import Grid from '@material-ui/core/Grid';
-// import IconButton from '@material-ui/core/IconButton';
-// import InputBase from '@material-ui/core/InputBase';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import Paper from '@material-ui/core/Paper';
-// import Radio from '@material-ui/core/Radio';
-// import RadioGroup from '@material-ui/core/RadioGroup';
-// import SearchIcon from '@material-ui/icons/Search';
 import Select from '@material-ui/core/Select';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-// import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-// import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { TableHead } from '@material-ui/core';
@@ -311,23 +291,23 @@ const HashtagView = (props) => {
               <TableBody>
                 <TableRow>
                   <TableCell>left</TableCell>
-                  <TableCell align="right">{props.stats.hashtag.manual.left}</TableCell>
+                  <TableCell align="right">{props.stats.hashtag.category.left}</TableCell>
                 </TableRow>
                 <TableRow>
                   <TableCell>neutral</TableCell>
-                  <TableCell align="right">{props.stats.hashtag.manual.neutral}</TableCell>
+                  <TableCell align="right">{props.stats.hashtag.category.neutral}</TableCell>
                 </TableRow>
                 <TableRow>
                   <TableCell>right</TableCell>
-                  <TableCell align="right">{props.stats.hashtag.manual.right}</TableCell>
+                  <TableCell align="right">{props.stats.hashtag.category.right}</TableCell>
                 </TableRow>
                 <TableRow>
                   <TableCell>positive</TableCell>
-                  <TableCell align="right">{props.stats.hashtag.manual.positive}</TableCell>
+                  <TableCell align="right">{props.stats.hashtag.category.positive}</TableCell>
                 </TableRow>
                 <TableRow>
                   <TableCell>negative</TableCell>
-                  <TableCell align="right">{props.stats.hashtag.manual.negative}</TableCell>
+                  <TableCell align="right">{props.stats.hashtag.category.negative}</TableCell>
                 </TableRow>
               </TableBody>
             </Table>
@@ -337,10 +317,10 @@ const HashtagView = (props) => {
 
           <FormGroup>
             <FormControl >
-              <InputLabel id="category-manual">CATEGORY</InputLabel>
+              <InputLabel id="category">CATEGORY</InputLabel>
               <Select
-                labelId="category-manual-label"
-                id="category-manual"
+                labelId="category-label"
+                id="category"
                 name="category"
                 className={clsx(classes.category, getCategoryClass(props.hashtag.category))} 
                 align="center"

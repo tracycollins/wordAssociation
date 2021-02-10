a30da879b5c236d373e780c16ecef654861589e4
tracycollins <tc@threeceemedia.com>
2021-02-10 01:51:29 -0500

    package

--- wordAssoServer/overlay/src/InfoOverlay.js
+++ wordAssoServer/overlay/src/InfoOverlay.js
@@ -1,25 +1,11 @@
 /* global MaterialUI,config,d3,HashMap,store,moment,io,ViewForceLinks,React,ReactDOM,InfoOverlay,ControlOverlay */
 const useStyles = MaterialUI.makeStyles((theme) => ({
-  root: {
-    border: 0,
-    width: "100%",
-    margin: theme.spacing(1),
-    padding: theme.spacing(1)
-  },
-  paragraph: {
-    margin: theme.spacing(1),
-    padding: theme.spacing(1)
-  },
   legend: {
-    // width: "50%",
-    margin: theme.spacing(1),
-    padding: theme.spacing(1)
+    // width: "80%",
   }
 }));
 
 const InfoOverlay = function(props){
-
-  // console.log(props)
 
   const Container = MaterialUI.Container;
   const Card = MaterialUI.Card;
@@ -34,29 +20,13 @@ const InfoOverlay = function(props){
   const classes = useStyles();
 
   return (
-    <Container className={classes.root}>
-      <Paper
-        varient="outlined"
-        aria-label="outlined"
-        component="div"
-      >
-
-        <Card className={classes.legend} variant="outlined">
-          <Icon
-            className={classes.paragraph}
-            onClick={props.closeButtonHandler}
-          >
-            cancel
-          </Icon>
-          <CardMedia
-            className={classes.legend}
-            src={"info_legend.png"}
-            component="img"
-            onError={e => {}}              
-          />
-        </Card>
-
-      </Paper>
-    </Container>
+    <Card className={classes.legend} variant="outlined">
+      <CardMedia
+        // className={classes.media}
+        src={"/public/assets/images/info_legend.png"}
+        component="img"
+        onError={e => {}}              
+      />
+    </Card>
   );
 }
